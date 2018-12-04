bc9c798fa944ec91b2f8b160df09bedc29ce304a
Zach Mullen <zach.mullen@kitware.com>
2018-12-04 11:11:35 -0500

    P4, part 2

--- p4.py
+++ p4.py
@@ -2,59 +2,45 @@ from collections import defaultdict
 import datetime
 import numpy as np
 
-AWAKE = 0
-ASLEEP = 1
-START = 2
-
 
 class Event(object):
-    def __init__(self, type, when, who):
-        self.type = type
-        self.when = when
-        self.who = who
+    def __init__(self, l):
+        date, time, msg = l.split(' ', 2)
+        y, M, d = (int(v) for v in date[1:].split('-'))
+        h, m = (int(v) for v in time[:-1].split(':'))
+        self.type = 'wake'
+        self.when = datetime.datetime(year=y, month=M, day=d, hour=h, minute=m)
+        self.who = None
 
-
-def get_event(l):
-    date, time, msg = l.split(' ', 2)
-    year, month, day = date[1:].split('-')
-    hour, min = time[:-1].split(':')
-    when = datetime.datetime(
-        year=int(year), month=int(month), day=int(day), hour=int(hour), minute=int(min))
-    who = None
-
-    if msg.startswith('Guard'):
-        type = START
-        who = int(msg.split()[1][1:])
-    elif msg.startswith('falls asleep'):
-        type = ASLEEP
-    else:
-        type = AWAKE
-
-    return Event(type=type, when=when, who=who)
+        if msg.startswith('Guard'):
+            self.type = 'start'
+            self.who = int(msg.split()[1][1:])
+        elif msg.startswith('falls asleep'):
+            self.type = 'sleep'
 
 
 counts = defaultdict(lambda: 0)
-events = sorted((get_event(l) for l in open('input4.txt').readlines()), key=lambda e: e.when)
+events = sorted((Event(l) for l in open('input4.txt').readlines()), key=lambda e: e.when)
 for e in events:
     if e.who is None:
         e.who = g
     else:
         g = e.who
-
-    if e.type == ASLEEP:
+    if e.type == 'sleep':
         asleep = e.when
-    elif e.type == AWAKE:
+    elif e.type == 'wake':
         counts[g] += (e.when - asleep).seconds / 60
 
-guard = max(counts, key=counts.__getitem__)
-minstogram = np.zeros((60,))
-start = 0
-for e in events:  # Find which minute the sleepy guard sleeps the most
-    if e.who != guard:
-        continue
-    if e.type == ASLEEP:
+minstogram = np.zeros((len(counts), 60), dtype=np.uint16)
+keys = {k: i for i, k in enumerate(counts)}
+for e in events:
+    if e.type == 'sleep':
         start = e.when.minute
-    elif e.type == AWAKE:
-        minstogram[start:e.when.minute] += 1
+    elif e.type == 'wake':
+        minstogram[keys[e.who], start:e.when.minute] += 1
 
-print(guard * np.argmax(minstogram))
+guard, minute = np.unravel_index(minstogram.argmax(), minstogram.shape)
+for g, i in keys.items():
+    if i == guard:
+        break
+print(minute * g)
